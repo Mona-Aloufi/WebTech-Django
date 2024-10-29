64b37c88434cd0e4aa313aaf81ba85447a4a533f
Pixel <pixel@webmail.com>
2024-10-29 22:54:08 +0300

    Lab 6

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -46,3 +46,34 @@ def listing_page(request):
 
 def table_page(request):
     return render(request, 'bookmodule/table.html')
+
+def search_page(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword', '').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+
+        # Filter the books based on search criteria
+        books = __getBooksList()
+        newBooks = []
+        
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower():
+                contained = True
+            if not contained and isAuthor and string in item['author'].lower():
+                contained = True
+            if contained:
+                newBooks.append(item)
+
+        # Render the filtered list of books
+        return render(request, 'bookmodule/bookList.html', {'books': newBooks})
+
+    # Render the search page for GET requests
+    return render(request, 'bookmodule/search.html')
+
+def __getBooksList():
+ book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+ book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+ book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+ return [book1, book2, book3]
