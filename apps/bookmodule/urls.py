542676d9cebda6f0f99d346da60980727d2f3de6
Pixel <pixel@webmail.com>
2024-10-29 22:22:14 +0300

    Lab 5 (HTML Part 2)

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -3,9 +3,12 @@ from . import views
 urlpatterns = [
  path('index2/<int:val1>/', views.index2),
  path('<int:bookId>', views.viewbook),
- path('task1lap5', views.task1lap5),
 
- 
+ path('html5/links', views.links_page, name='books.html5.links'),
+ path('html5/text/formatting', views.text_formatting_page, name="text_formatting"),
+ path('html5/listing', views.listing_page, name="listing_page"),
+ path('html5/tables', views.table_page, name="table_page"),
+
 path('', views.index, name="books.index"),
 path('list_books/', views.list_books, name="books.list_books"),
 path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
